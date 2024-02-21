f1d0a20e5686f9aa8ce5933a017b88d1fa3d0dae
VIJAYAKUMARAN03 <vijayakumaran2k3@gmail.com>
2024-02-21 14:22:19 +0530

    CORS added and Reponse Statements are updated

--- Fetch_api/fetch_bmp.py
+++ Fetch_api/fetch_bmp.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
-UPLOAD_FOLDER = 'uploads'  # Folder where the uploaded files will be stored
+UPLOAD_FOLDER = 'uploads'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
@@ -11,24 +13,23 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    # Check if the POST request has the file part
     if 'file' not in request.files:
         return 'No file in the Request'
-    
+
     file = request.files['file']
 
-    # If the user does not select a file, the browser submits an empty file without a filename
     if file.filename == '':
         return 'No file selected'
 
     if file.filename[-4:] != '.bmp':
         return 'Selected file was not in .bmp type'
-    # If file is successfully received, save it to the UPLOAD_FOLDER
+
     filename = file.filename
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     file.save(file_path)
-    
-    return f'File {filename} successfully received and saved at {file_path}'
+
+    return f'File {filename} successfully received and saved to ArmBoard'
+    # return f'File {filename} successfully received and saved at {file_path}'
 
 if __name__ == '__main__':
     app.run(debug=True)
