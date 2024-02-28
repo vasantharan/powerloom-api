db86dbc28eeaf7f4a6e2713c7576470c92d783d3
VIJAYAKUMARAN03 <vijayakumaran2k3@gmail.com>
2024-02-28 14:40:52 +0530

    api functions updated

--- Fetch_api/fetch_bmp.py
+++ Fetch_api/fetch_bmp.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+# from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -11,12 +12,18 @@ if not os.path.exists(UPLOAD_FOLDER):
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# Connect to MongoDB
+# client = MongoClient('mongodb://localhost:27017/')
+# db = client['powerloom_db']
+# collection = db['powerloom_collection']
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
         return 'No file in the Request'
 
     file = request.files['file']
+    printing_count = request.form.get('printingCount', type=int)  # Get printing count from request
 
     if file.filename == '':
         return 'No file selected'
@@ -28,8 +35,15 @@ def upload_file():
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     file.save(file_path)
 
-    return f'File {filename} successfully received and saved to ArmBoard'
-    # return f'File {filename} successfully received and saved at {file_path}'
+    # Store printing count data in MongoDB
+    document = {
+        'filename': filename,
+        'printing_count': printing_count,
+        'file_path': file_path
+    }
+    # collection.insert_one(document)
+
+    return (f'File {filename} successfully received and saved to ArmBoard with printing count of {printing_count}')
 
 if __name__ == '__main__':
     app.run(debug=True)
